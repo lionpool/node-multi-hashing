b5265c74ecece6be623c88486c02e577609f3d50
Joan Bellés <nosekefik@gmail.com>
2017-10-12 20:59:43 +0200

    changes for neoscrypt

--- neoscrypt.h
+++ neoscrypt.h
@@ -1,13 +1,10 @@
-#if (__cplusplus)
+#ifdef __cplusplus
 extern "C" {
 #endif
-
-void neoscrypt(const unsigned char *input, unsigned char *output, unsigned int profile);
-
-#if (__cplusplus)
+void neoscrypt(const char *input, char *output, int profile);
+#ifdef __cplusplus
 }
 #else
-
 #define SCRYPT_BLOCK_SIZE 64
 #define SCRYPT_HASH_BLOCK_SIZE 64
 #define SCRYPT_HASH_DIGEST_SIZE 32
@@ -30,4 +27,3 @@ typedef uint8_t hash_digest[SCRYPT_HASH_DIGEST_SIZE];
     U32TO8_BE((p) + 4, (uint32_t)((v)      ));
 
 #endif
-
