de1ca397ad94b3eff663e044cbc2cef4cd25c8fa
lionpool <35387777+lionpool@users.noreply.github.com>
2018-01-16 05:53:15 +0200

    Update x14.h

--- x14.h
+++ x14.h
@@ -1,16 +1,5 @@
-#ifndef X14_H
-#define X14_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <stdint.h>
-
-void x14_hash(const char* input, char* output, uint32_t len);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+##pragma once                                                                                                                                                                              
+                                                                                                                                                                                          
+#include <stdint.h>                                                                                                                                                                       
+                                                                                                                                                                                          
+void x14_hash(const char* input, char* output, uint32_t len); 
