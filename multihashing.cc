5dbb598c2161dd12d0cb3f6aa54444990f524618
lionpool <35387777+lionpool@users.noreply.github.com>
2018-01-16 05:43:21 +0200

    Update multihashing.cc

--- multihashing.cc
+++ multihashing.cc
@@ -515,6 +515,26 @@ NAN_METHOD(sha1) {
     info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
 }
 
+NAN_METHOD(x14) {
+
+    if (info.Length() < 1)
+        return THROW_ERROR_EXCEPTION("You must provide one argument.");
+
+    Local<Object> target = Nan::To<Object>(info[0]).ToLocalChecked();
+
+    if(!Buffer::HasInstance(target))
+        return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char *output = (char*) malloc(sizeof(char) * 32);
+
+    uint32_t input_len = Buffer::Length(target);
+
+    x14_hash(input, output, input_len);
+
+    info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
+}
+
 NAN_METHOD(x15) {
 
     if (info.Length() < 1)
